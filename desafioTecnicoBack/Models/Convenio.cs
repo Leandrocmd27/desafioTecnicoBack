7e119cd6a6243910f54fb0294ca9c61e480233e0
leandro.dias@tradeTechnology.com.br <leandro.dias@tradeTechnology.com.br>
2025-06-01 12:21:44 -0300

    Adicionar arquivos de projeto.

---
+++ desafioTecnicoBack/Models/Convenio.cs
@@ -0,0 +1,8 @@
+﻿namespace desafioTecnicoBack.Models
+{
+    public class Convenio
+    {
+        public int ConvenioId { get; set; }
+        public string NomeConvenio { get; set; }
+    }
+}
---
+++ desafioTecnicoBack/Models/Paciente.cs
@@ -0,0 +1,22 @@
+﻿namespace desafioTecnicoBack.Models
+{
+    public class Paciente
+    {
+        public int PacienteId { get; set; }
+        public string? Nome { get; set; }
+        public string? SobreNome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string? Genero { get; set; }
+        public string? CPF { get; set; }
+        public string? RG { get; set; }
+        public string? UFRG { get; set; }
+        public string? Email { get; set; }
+        public string? Celular { get; set; }
+        public string? TelefoneFixo { get; set; }
+        public int ConvenioId { get; set; }
+        public Convenio? Convenio { get; set; }
+        public string? NumeroCarteirinha { get; set; }
+        public string? ValidadeCarteirinha { get; set; }
+
+    }
+}
